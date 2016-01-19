558d69ebad83da79dc11269cb1eb0eb30b5d5bc5
Jennie Buechner <janina.buechner@gmail.com>
2016-01-19 09:50:40 -0800

    create models and associations

---
+++ db/schema.rb
@@ -0,0 +1,19 @@
+ActiveRecord::Schema.define(version: 20160119173807) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.decimal  "price"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "products", ["category_id"], name: "index_products_on_category_id"
+
+end
